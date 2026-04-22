fdf29f2cffc90e4c254f47a552b56bca6c470bab
ads58 <ads58@W10792>
2026-04-22 19:54:51 +0700

    feature(Model): внесены правки

--- lb1/View/MainForm.cs
+++ lb1/View/MainForm.cs
@@ -294,8 +294,7 @@ namespace View
         /// </summary>
         /// <param name="filePath">Путь к файлу для загрузки</param>
         /// <returns>Список загруженных фигур типа <see cref="IShape"/></returns>
-        /// <exception cref="Exception"
-        /// >Выбрасывается при ошибке десериализации</exception>
+        /// <exception cref="Exception">Выбрасывается при ошибке десериализации</exception>
         private List<IShape> DeserializeShapes(string filePath)
         {
             try
