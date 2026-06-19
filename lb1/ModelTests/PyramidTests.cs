e58fdc273ab6fde694d67fcc896c51219db014d1
Lenovo <aleksandersklyaruk@yandex.ru>
2026-06-20 01:18:47 +0700

    Внесены изменения в PyramidTests

--- lb1/ModelTests/PyramidTests.cs
+++ lb1/ModelTests/PyramidTests.cs
@@ -1,13 +1,16 @@
 ﻿namespace ModelTests
 {
+    /// <summary>
+    /// Набор модульных тестов для класса Pyramid
+    /// </summary>
     public class PyramidTests
     {
 
         /// <summary>
-        /// Проверка инициализации свойства Name (один Assert, обычный Test).
+        /// Проверка инициализации свойства Name
         /// </summary>
         [Test]
-        public void Constructor_ValidParameters_ShouldSetName()
+        public void ConstructorValidParametersShouldSetName()
         {
             var pyramid = new Pyramid(3.0, 4.0, 5.0);
 
@@ -15,12 +18,18 @@
         }
 
         /// <summary>
-        /// Проверка сохранения Length через TestCase (один Assert, много данных).
+        /// Проверяет, что конструктор корректно 
+        /// сохраняет значение длины основания
         /// </summary>
+        /// <param name="length">Длина основания</param>
+        /// <param name="width">Ширина основания</param>
+        /// <param name="height">Высота пирамиды</param>
+        /// <param name="expectedLength">Ожидаемое значение длины</param>
         [TestCase(3.0, 4.0, 5.0, 3.0)]
         [TestCase(1.5, 2.5, 3.5, 1.5)]
-        public void Constructor_ValidParameters_ShouldAssignLength(
-            double length, double width, double height, double expectedLength)
+        public void ConstructorValidParametersShouldAssignLength(
+            double length, double width, 
+            double height, double expectedLength)
         {
             var pyramid = new Pyramid(length, width, height);
 
@@ -28,35 +37,47 @@
         }
 
         /// <summary>
-        /// Проверка валидации через TestCase (один Assert на выброс исключения).
+        /// Проверяет, что конструктор выбрасывает ArgumentException
+        /// при передаче некорректных параметров
         /// </summary>
+        /// <param name="length">Длина основания</param>
+        /// <param name="width">Ширина основания</param>
+        /// <param name="height">Высота пирамиды</param>
         [TestCase(0, 4, 5)]
         [TestCase(3, -1, 5)]
         [TestCase(3, 4, double.NaN)]
         [TestCase(double.PositiveInfinity, 4, 5)]
-        public void Constructor_InvalidParameters_ShouldThrowArgumentException(
+        public void ConstructorInvalidParametersShouldThrowArgumentException(
             double length, double width, double height)
         {
-            Assert.Throws<ArgumentException>(() => new Pyramid(length, width, height));
+            Assert.Throws<ArgumentException>(() => 
+            new Pyramid(length, width, height));
         }
 
         /// <summary>
-        /// Расчёт объёма по формуле V = 1/3 * a * b * h.
-        /// Идеальный кандидат для TestCase с одним Assert.
+        /// Проверяет корректность расчёта объёма пирамиды по формуле 
+        /// V = 1/3 × a × b × h
         /// </summary>
-        [TestCase(3.0, 4.0, 5.0, 20.0)]             // 1/3 * 3 * 4 * 5 = 20
-        [TestCase(6.0, 6.0, 6.0, 72.0)]             // 1/3 * 6 * 6 * 6 = 72
-        [TestCase(1.0, 1.0, 1.0, 0.333333333333333)] // 1/3
-        public void CalculateVolume_ValidParameters_ShouldReturnCorrectVolume(
-            double length, double width, double height, double expectedVolume)
+        /// <param name="length">Длина основания</param>
+        /// <param name="width">Ширина основания</param>
+        /// <param name="height">Высота пирамиды</param>
+        /// <param name="expectedVolume">Ожидаемый объём</param>
+        [TestCase(3.0, 4.0, 5.0, 20.0)]           
+        [TestCase(6.0, 6.0, 6.0, 72.0)]             
+        [TestCase(1.0, 1.0, 1.0, 0.333333333333333)] 
+        public void CalculateVolumeValidParametersShouldReturnCorrectVolume(
+            double length, double width, 
+            double height, double expectedVolume)
         {
             var pyramid = new Pyramid(length, width, height);
 
-            Assert.That(pyramid.CalculateVolume(), Is.EqualTo(expectedVolume).Within(Settings.Tolerance));
+            Assert.That(pyramid.CalculateVolume(), 
+                Is.EqualTo(expectedVolume).Within(Settings.Tolerance));
         }
 
         /// <summary>
-        /// Проверка базового свойства Radius (возвращает 0).
+        /// Проверяет, что свойство Pyramid.Radius возвращает ноль,
+        /// так как пирамида не переопределяет это свойство
         /// </summary>
         [Test]
         public void Radius_ShouldReturnZero()
@@ -67,49 +88,56 @@
         }
 
         /// <summary>
-        /// Форматирование строки Parameters через TestCase.
+        /// Проверяет, что свойство Pyramid.Parameters 
+        /// возвращает строку в корректном формате
         /// </summary>
+        /// <param name="length">Длина основания</param>
+        /// <param name="width">Ширина основания</param>
+        /// <param name="height">Высота пирамиды</param>
         [TestCase(3.0, 4.0, 5.0)]
         [TestCase(1.5, 2.5, 3.5)]
-        public void Parameters_ValidParameters_ShouldReturnCorrectFormat(
+        public void ParametersValidParametersShouldReturnCorrectFormat(
             double length, double width, double height)
         {
             var pyramid = new Pyramid(length, width, height);
-            string expected = $"Длина = {length:F2}\nШирина = {width:F2}\nВысота = {height:F2}";
+            string expected = $"Длина = {length:F2}\n" +
+                $"Ширина = {width:F2}\nВысота = {height:F2}";
 
             Assert.That(pyramid.Parameters, Is.EqualTo(expected));
         }
 
         /// <summary>
-        /// Форматирование строки GetInfo через TestCase.
+        /// Проверяет, что метод Pyramid.GetInfo 
+        /// возвращает строку в корректном формате
         /// </summary>
+        /// <param name="length">Длина основания</param>
+        /// <param name="width">Ширина основания</param>
+        /// <param name="height">Высота пирамиды</param>
         [TestCase(3.0, 4.0, 5.0)]
-        public void GetInfo_ValidParameters_ShouldReturnCorrectFormat(
+        public void GetInfoValidParametersShouldReturnCorrectFormat(
             double length, double width, double height)
         {
             var pyramid = new Pyramid(length, width, height);
             double volume = pyramid.CalculateVolume();
 
-            // ВАЖНО: Здесь учтена специфическая форматировка из вашего Pyramid.cs 
-            // (запятые в начале новых строк после \n)
-            string expected = $"Длина = {length:F2}\n" +
-                              $", Ширина = {width:F2}\n" +
-                              $", Высота = {height:F2}\n" +
-                              $", Объём = {volume:F2}";
+            string expected = $"Длина = {length:F2}\n, " +
+                $"Ширина = {width:F2}\n, Высота = {height:F2}\n" +
+                $", Объём = {volume:F2}";
 
             Assert.That(pyramid.GetInfo(), Is.EqualTo(expected));
         }
 
+        /// <summary>
+        /// Проверяет, что при конвертации пирамиды в ShapeData
+        /// свойство ShapeData.Radius равно нулю
+        /// </summary>
         [Test]
-        public void FromShape_RadiusShouldSerializeCorrectly()
+        public void FromShapeRadiusShouldSerializeCorrectly()
         {
-            // Arrange
             var pyramid = new Pyramid(3, 3, 3);
 
-            // Act
             var data = ShapeData.FromShape(pyramid);
 
-            // Assert
             Assert.That(data.Radius, Is.EqualTo(0));
         }
     }
