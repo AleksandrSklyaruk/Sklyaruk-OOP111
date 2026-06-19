7833d67c65aa51f66ef0646e9ad1ae494fa7c0e7
Lenovo <aleksandersklyaruk@yandex.ru>
2026-06-20 01:04:38 +0700

    Внесены правки в SphereTests

--- lb1/ModelTests/Settings.cs
+++ lb1/ModelTests/Settings.cs
@@ -1,12 +1,12 @@
 ﻿namespace ModelTests
 {
     /// <summary>
-    /// 
+    /// Статический класс, содержащий общие настройки для модульных тестов
     /// </summary>
     internal static class Settings
     {
         /// <summary>
-        /// Допустимая погрешность для сравнения числовых значений типа double.
+        /// Допустимая погрешность для сравнения числовых значений типа double
         /// </summary>
         internal const double Tolerance = 1e-6;
     }
--- lb1/ModelTests/SphereTests.cs
+++ lb1/ModelTests/SphereTests.cs
@@ -1,123 +1,144 @@
 ﻿namespace ModelTests
 {
-
+    /// <summary>
+    /// Набор модульных тестов для класса Sphere
+    /// </summary>
     public class SphereTests
     {
+        /// <summary>
+        /// Проверяет, что конструктор корректно инициализирует 
+        /// свойство Sphere.Name
+        /// </summary>
         [Category("Name Sphere")]
-        // Обычный тест (аналог [Fact] в xUnit)
         [Test]
-        public void Constructor_ValidRadius_ShouldInitializeCorrectly()
+        public void ConstructorValidRadiusShouldInitializeCorrectly()
         {
             Sphere sphere = new Sphere(5.0);
+
             Assert.AreEqual("Шар", sphere.Name);
         }
 
+        /// <summary>
+        /// Проверяет, что конструктор выбрасывает ArgumentException 
+        /// при передаче некорректного радиуса
+        /// </summary>
+        /// <param name="invalidRadius">Некорректное значение радиуса</param>
         [Category("Validation Sphere")]
-        // Параметризованный тест (аналог [Theory] в xUnit)
-        // Тест запустится 4 раза с разными значениями
         [TestCase(0)]
         [TestCase(-1.0)]
         [TestCase(double.NaN)]
         [TestCase(double.PositiveInfinity)]
         [TestCase(double.NegativeInfinity)]
-        public void Constructor_InvalidRadius_ShouldThrowArgumentException(double invalidRadius)
+        public void ConstructorInvalidRadiusShouldThrowArgumentException
+            (double invalidRadius)
         {
             Assert.Throws<ArgumentException>(() => new Sphere(invalidRadius));
         }
 
-        // Arrange - input
+        /// <summary>
+        /// Проверяет корректность расчёта объёма шара по формуле 
+        /// V = 4/3 × π × r³
+        /// </summary>
+        /// <param name="radius">Радиус шара</param>
+        /// <param name="expectedVolume">Ожидаемый объём</param>
         [TestCase(3.0, 113.09733552923255)]
         [TestCase(1.0, 4.1887902047863905)]
         public void CalculateVolume_ValidRadius_ReturnsCorrectVolume(
-                    double radius,
-                    double expectedVolume)
+            double radius,
+            double expectedVolume)
         {
             Sphere sphere = new Sphere(radius);
+
             double actualVolume = sphere.CalculateVolume();
 
-            Assert.That(
-                actualVolume,
+            Assert.That( actualVolume, 
                 Is.EqualTo(expectedVolume).Within(Settings.Tolerance));
         }
 
+        /// <summary>
+        /// Проверяет, что метод Sphere.GetInfo 
+        /// возвращает строку в корректном формате
+        /// </summary>
         [Category("GetInfo")]
         [Test]
         public void GetInfoReturnsCorrectFormat()
         {
-            // Arrange
             double radius = 5.0;
             Sphere sphere = new Sphere(radius);
-            // Если у вас есть вспомогательный метод, можно использовать его:
-            // Sphere sphere = CreateDefaultSphere();
 
-            // Act
             string actualInfo = sphere.GetInfo();
 
-            // Assert
-            double expectedVolume = (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+            double expectedVolume = (4.0 / 3.0) * Math.PI * 
+                Math.Pow(radius, 3);
             string expectedInfo = $"Радиус = {radius:F2}, " +
                 $"Объём = {expectedVolume:F2}";
 
             Assert.That(actualInfo, Is.EqualTo(expectedInfo));
         }
 
-        [Category("Radius Format")]
+        /// <summary>
+        /// Проверяет, что свойство Sphere.Parameters 
+        /// возвращает строку в корректном формате
+        /// </summary>
+        [Category("Parameters")]
         [Test]
         public void GetInfoReturnsCorrectParameters()
         {
-            // Arrange
             double radius = 5.0;
             Sphere sphere = new Sphere(radius);
-            // Если у вас есть вспомогательный метод, можно использовать его:
-            // Sphere sphere = CreateDefaultSphere();
 
-            // Act
             string actualInfo = sphere.Parameters;
 
-            // Assert
-            double expectedVolume = (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+            double expectedVolume = (4.0 / 3.0) * Math.PI * 
+                Math.Pow(radius, 3);
             string expectedInfo = $"Радиус = {radius:F2}";
 
             Assert.That(actualInfo, Is.EqualTo(expectedInfo));
         }
 
+        /// <summary>
+        /// Проверяет, что свойство Sphere.Radius возвращает значение,
+        /// переданное в конструктор
+        /// </summary>
+        /// <param name="radius">Радиус шара</param>
         [Category("Radius")]
         [TestCase(1.0)]
         [TestCase(5.5)]
         [TestCase(100.0)]
         [TestCase(0.001)]
-        public void Constructor_ValidRadius_ShouldInitializeCorrectly(double radius)
+        public void ConstructorValidRadiusShouldInitializeCorrectly
+            (double radius)
         {
-            // Act
             Sphere sphere = new Sphere(radius);
 
-            // Assert
             Assert.That(sphere.Radius, Is.EqualTo(radius));
         }
 
+        /// <summary>
+        /// Проверяет, что при конвертации шара в ShapeData
+        /// свойство ShapeData.Height равно нулю
+        /// </summary>
         [Test]
-        public void FromShape_Height_ShouldSerializeCorrectly()
+        public void FromShapeHeightShouldSerializeCorrectly()
         {
-            // Arrange
             var sphere = new Sphere(3);
 
-            // Act
             var data = ShapeData.FromShape(sphere);
 
-            // Assert
             Assert.That(data.Height, Is.EqualTo(0));
         }
 
+        /// <summary>
+        /// Проверяет, что при конвертации шара в ShapeData
+        /// свойство ShapeData.Width равно нулю.
+        /// </summary>
         [Test]
-        public void FromShape_Width_ShouldSerializeCorrectly()
+        public void FromShapeWidthShouldSerializeCorrectly()
         {
-            // Arrange
             var sphere = new Sphere(3);
 
-            // Act
             var data = ShapeData.FromShape(sphere);
 
-            // Assert
             Assert.That(data.Width, Is.EqualTo(0));
         }
     }
